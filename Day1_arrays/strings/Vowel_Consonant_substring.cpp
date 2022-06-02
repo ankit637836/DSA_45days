98ec7a4f243beab3a8ba143b3a9001cc3d721a85
ankitIITRPR <94774111+ankitIITRPR@users.noreply.github.com>
2022-06-02 23:15:02 +0530

    Create Vowel_Consonant_substring.cpp

---
+++ Day1_arrays/strings/Vowel_Consonant_substring.cpp
@@ -0,0 +1,24 @@
+int Solution::solve(string A) {
+    long int  l = A.length();
+    long int mod = 1000000007;
+
+    int vowel =0, cons =0;
+    long int sum=0;
+
+    for(int i=0;i<l;i++){
+        A[i] = tolower(A[i]);
+            if(A[i]=='a' || A[i]=='e' || A[i]=='i'|| A[i]=='o'|| A[i]=='u'){
+                sum += cons;
+                vowel++;
+            }
+            else{
+                sum += vowel;
+                cons++;
+            }
+
+        sum%=mod;
+    }
+
+    return sum;
+}
+
