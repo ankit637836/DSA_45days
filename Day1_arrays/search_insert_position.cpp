cb8666fa6cc2650a67a6d086aa99382b08d0d58b
ankitIITRPR <94774111+ankitIITRPR@users.noreply.github.com>
2022-06-02 13:01:48 +0530

    Update search_insert_position.cpp

--- Day1_arrays/search_insert_position.cpp
+++ Day1_arrays/search_insert_position.cpp
@@ -1,3 +1,14 @@
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+ // } Driver Code Ends
+// User function template for C++
+
 class Solution {
 public:
     int searchInsert(vector<int>& a, int b) {
@@ -19,3 +30,20 @@ public:
         return s;
     }
 };
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> array(n - 1);
+        for (int i = 0; i < n - 1; ++i) cin >> array[i];
+        Solution obj;
+        cout << obj.MissingNumber(array, n) << "\n";
+    }
+    return 0;
+}  // } Driver Code Ends
