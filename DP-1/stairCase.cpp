0f6eda9f59f5f4ff1a439a187e1dbd8b6ac71b71
ankitIITRPR <94774111+ankitIITRPR@users.noreply.github.com>
2022-06-24 18:06:26 +0530

    Create stairCase.cpp

---
+++ DP-1/stairCase.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+long long M = 1e9 +7;
+
+void solve(){
+	int n;
+	cin>>n;
+
+	int* arr = new int[n+1];
+
+	arr[0]=1;
+	arr[1]=1;
+	arr[2]=2;
+
+	for(int i=3;i<=n;i++){
+		arr[i] = arr[i-1] + arr[i-2] + arr[i-3];
+	}
+	int ans = arr[n];
+	delete [] arr;
+	cout<<ans;
+
+}
+int main()
+{
+	int t;
+	//cin >> t;
+	t=1;
+	while (t--)
+	{
+		solve();
+	}
+}
