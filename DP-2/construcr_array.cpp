def250e6d2b151e99d24dad9a7a47eab8dd8d60f
ankitIITRPR <94774111+ankitIITRPR@users.noreply.github.com>
2022-06-25 14:54:21 +0530

    Update construcr_array.cpp

--- DP-2/construcr_array.cpp
+++ DP-2/construcr_array.cpp
@@ -5,9 +5,7 @@ long countArray(int n, int k, int x) {
     // Return the number of ways to fill in the array.
     long oneC = 1;
     long nonOneC = 0;
-    int arr[n];
-    arr[0] = 1;
-    arr[n-1] = x;
+    
     for(int i=1;i<n;i++){
         long prevC = oneC;
         oneC = (nonOneC*(k-1))%M;
